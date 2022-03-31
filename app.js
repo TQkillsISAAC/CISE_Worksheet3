02f7ff73706cfb9b96d1df9acbe6c284a919e77e
TQkillsISAAC <gdn5850@autuni.ac.nz>
2022-04-01 03:00:42 +1300

    Eighth Commit
    Test heroku

--- app.js
+++ app.js
@@ -88,6 +88,15 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
+app.use(express.static("build"));
+
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./mern-app/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./mern-app/build", "index.html"));
+});
+
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
