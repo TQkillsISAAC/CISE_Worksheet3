769fe7c46e82647a27d6793ae5dd6885072e3af8
TQkillsISAAC <gdn5850@autuni.ac.nz>
2022-03-31 22:12:41 +1300

    commit message

--- app.js
+++ app.js
@@ -1,64 +1,111 @@
-// mongoose
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
+// // mongoose
+// const mongoose = require('mongoose');
+// const config = require('config');
+// const db = config.get('mongoURI');
+// const bodyParser = require("body-parser");
+// require("dotenv").config();
+
+// const path = require("path");
+
+// // app.js
+
+// const express = require('express');
+// const connectDB = require('./config/db');
+// // var cors = require('cors');
+// const cors = require('cors');
+
+// // routes
+// const books = require('./routes/api/books');
+
+// const app = express();
+// app.use(cors());
+
+// //import your models
+// require("./models/Book");
+
+// // Connect Database
+// connectDB();
+
+// require("dotenv").config()
+
+//     mongoose
+//      .connect(
+//          process.env.MONGODB_CONNECTION_STRING,
+//              {
+//                useNewUrlParser: true,
+//                useUnifiedTopology: true,
+//              }
+//      )
+//      .then(() => console.log("MongoDB has been connected"))
+//      .catch((err) => console.log(err));
+
+  
+// // cors
+// app.use(cors({ origin: true, credentials: true }));
+
+// // Init Middleware
+// app.use(express.json({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+
+// app.get('/', (req, res) => res.send('Hello world!'));
+
+// // use Routes
+// app.use('/api/books', books);
+
+// const port = process.env.PORT || 8082;
+
+// app.use(express.static(path.resolve(__dirname, "./mern-app/build")));
+
+// app.get("*", function (request, response) {
+//     response.sendFile(path.resolve(__dirname, "./mern-app/build", "index.html"));
+//   });
+
+//   app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const path = require("path");
-
-// app.js
-
-const express = require('express');
-const connectDB = require('./config/db');
-// var cors = require('cors');
-const cors = require('cors');
-
-// routes
-const books = require('./routes/api/books');
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 //import your models
-require("./models/Book");
+require("./models/Book.js");
 
-// Connect Database
-connectDB();
+mongoose
+  .connect(
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
-require("dotenv").config()
-
-    mongoose
-     .connect(
-         process.env.MONGODB_CONNECTION_STRING,
-             {
-               useNewUrlParser: true,
-               useUnifiedTopology: true,
-             }
-     )
-     .then(() => console.log("MongoDB has been connected"))
-     .catch((err) => console.log(err));
-
-  
-// cors
-app.use(cors({ origin: true, credentials: true }));
-
-// Init Middleware
-app.use(express.json({ extended: false }));
+//middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//import routes
+require("./routes/api/books.js")(app);
 
-// use Routes
-app.use('/api/books', books);
+const PORT = process.env.PORT || 8082;
 
-const port = process.env.PORT || 8082;
+// Accessing the path module
+const path = require("path");
 
+// Step 1:
 app.use(express.static(path.resolve(__dirname, "./mern-app/build")));
-
+// Step 2:
 app.get("*", function (request, response) {
-    response.sendFile(path.resolve(__dirname, "./mern-app/build", "index.html"));
-  });
+  response.sendFile(path.resolve(__dirname, "./mern-app/build", "index.html"));
+});
 
-  app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
